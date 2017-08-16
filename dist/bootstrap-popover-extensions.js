91dd82a4f0546e276f2ef9ed9aae6decc2dc7a06
Niels Holt <niels@steenbuchholt.dk>
2017-08-16 12:01:14 +0200

    Adjust to Bootstrap 4.0.0-beta and Popper.js

--- dist/bootstrap-popover-extensions.js
+++ dist/bootstrap-popover-extensions.js
@@ -8,7 +8,7 @@
 
 ****************************************************************************/
 
-(function ($, Tether/*, window, document, undefined*/) {
+(function ($/*, window, document, undefined*/) {
 	"use strict";
 	
     //Concert from all new placement to original
@@ -20,77 +20,46 @@
     };
 
     /****************************************************
-    Modify Tether.position to check if the element has a 
-    data('Tether.position')-function and use it
-    *****************************************************/
-    Tether.modules.push({
-        position: function( options ) { 
-            var $element = $(this.element),
-                posFunction = $element.data('Tether.position');
-
-            if (posFunction)
-                return $.proxy( posFunction, this )( options );              
-            else
-                return { top: options.top, left: options.left };
-        }
-    });
-
-    
-    
-    /****************************************************
-    bs_popover_position
-    Calculate and modifies positions for popover
-    *****************************************************/
-    function bs_popover_position( options ){
-        // +/-deltaY, +/-deltaX = distance to move popover to align edge with target
-        var deltaY = options.elementPos.height / 2,
-            deltaX = options.elementPos.width / 2,
-            result = { top: options.top, left: options.left },
-            placement = $(this.element).data( 'popver.truePlacement' );
-
-        switch (placement){
-          case "topleft"    : result.left -= deltaX; break;
-          case "topright"   : result.left += deltaX; break;
-          case "bottomleft" : result.left -= deltaX; break;
-          case "bottomright": result.left += deltaX; break;
-
-          case "lefttop"    : result.top -= deltaY; break;
-          case "leftbottom" : result.top += deltaY; break;
-          case "righttop"   : result.top -= deltaY; break;
-          case "rightbottom": result.top += deltaY; break;
-        }
-        return result;
-    }
-
-  
-    /****************************************************
     Overwrite Popover.show to save and modify new positions
     *****************************************************/
     $.fn.popover.Constructor.prototype.show = function( _show ){
         return function(){
-
-            //If first time: Save 'true' placement, add position-function to element and add new classes
+            //If first time: Save 'true' placement
             if (!this.config.truePlacement){
                 this.config.truePlacement = this.config.placement;
                 this.config.placement = truePlacement2placement[this.config.truePlacement];
-
-                if (this.config.placement != this.config.truePlacement)
-                    $(this.tip)
-                        .data( 'popver.truePlacement', this.config.truePlacement )
-                        .data( 'Tether.position', bs_popover_position )
-                        .addClass('popover-' + this.config.truePlacement );
             }
             
             //Original methods
             _show.apply(this, arguments);
 
+            //Adjust popover
+            var $tip       = $(this.tip),
+                arrowDim   = $tip.find('.arrow').width() || 10,
+                arrowOffset = 6 + arrowDim,
+                halfWidth  = $tip.width() / 2,
+                halfHeight = $tip.height() / 2,
+                offset     = 0;
+
+            switch (this.config.truePlacement){
+                case "topleft"    : 
+                case "bottomleft" : offset = -halfWidth + arrowOffset; break;
+
+                case "topright"   :
+                case "bottomright": offset =  halfWidth - arrowOffset; break;
+
+              
+                case "lefttop"    :
+                case "righttop"   : offset = -halfHeight + arrowOffset; break;
+
+                case "leftbottom": 
+                case "rightbottom": offset =  halfHeight - arrowOffset; break;
+            }
+
+            this._popper.modifiers[1].offset = offset;
         };
+
     }( $.fn.popover.Constructor.prototype.show );
     
-    
-    //Initialize/ready 
-	$(function() { 
-
-	
-	}); 
-}(jQuery, window.Tether, this, document));
+   
+}(jQuery, this, document));
